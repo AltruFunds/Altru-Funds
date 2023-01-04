6e54d56b3b05638726ef3514dabdacad4f353afd
Bassey Hyonam Victor <victorbassey69@yahoo.com>
2023-01-04 21:55:24 +0100

    ...further hosting checks

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>Altru funds</h1>
       <p>Getting started</p>
+      <p>Premium web based crowd funding platform</p>
+      <a href='/' target='_self'>Home | Admin </a>
     </div>
   );
 }
